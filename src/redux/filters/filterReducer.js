201cb606fb8b07815b010a7c269422ea4347097a
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2024-12-26 16:10:51 +0100

    Update filterReducer.js

--- src/redux/filters/filterReducer.js
+++ src/redux/filters/filterReducer.js
@@ -1,11 +1,16 @@
+import { createReducer } from "@reduxjs/toolkit";
 import { changeFilter } from "./actions";
 import { FILTERS } from "./consts";
 
-export const filterReducer = (state = FILTERS.ALL, action) => {
-    switch (action.type) {
-      case changeFilter.type:
-          return action.payload;    
-      default:
-        return state;
-  }
-};
+// export const filterReducer = (state = FILTERS.ALL, action) => {
+//     switch (action.type) {
+//       case changeFilter.type:
+//           return action.payload;
+//       default:
+//         return state;
+//   }
+// };
+
+export const filterReducer = createReducer(FILTERS.ALL, (builder) => {
+  builder.addCase(changeFilter, (state, action) => action.payload);
+});
